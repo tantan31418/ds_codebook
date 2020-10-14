86b8fd460e036ae0ce8c399d93edec4c7ee26410
tantan <tantan31418@gmail.com>
2020-10-14 16:36:08 +0800

    Do auto formatting

--- infix_to_postfix.cpp
+++ infix_to_postfix.cpp
@@ -3,74 +3,90 @@
  * Use stack to implement postfix convert
 */
 
-#include<iostream>
-#include<string>
-#include<stack>
+#include <iostream>
+#include <string>
+#include <stack>
 #define DEBUG 0
-
 
 using namespace std;
 
 string conver_posfx(string infx);
 int get_prec(char c);
 
-int main(){
-    string infx,posfx;
-    cin>>infx;
+int main()
+{
+    string infx, posfx;
+    cin >> infx;
     posfx = conver_posfx(infx);
-    cout<<posfx<<endl;
+    cout << posfx << endl;
 
     return 0;
 }
 
-int get_prec(char c){
-    if (c=='+'||c=='-'){
+int get_prec(char c)
+{
+    if (c == '+' || c == '-')
+    {
         return 1;
     }
-    else if (c=='*'||c=='\\'){
+    else if (c == '*' || c == '\\')
+    {
         return 2;
     }
-    else{
+    else
+    {
         return 0;
     }
 }
 
-int is_op(char c){
-    if (c=='+'||c=='-'||c=='*'||c=='\\'){
+int is_op(char c)
+{
+    if (c == '+' || c == '-' || c == '*' || c == '\\')
+    {
         return 1;
     }
-    else {
+    else
+    {
         return 0;
     }
 }
 
-
-string conver_posfx(string infx){
+string conver_posfx(string infx)
+{
     stack<char> st;
     infx = '(' + infx + ')'; //to initialize
     int s_len = infx.length();
     string post;
-    for (int i=0;i<s_len;i++){
-        if (DEBUG) cout<<"line53"<<endl;
-        if (infx[i]=='('){
+    for (int i = 0; i < s_len; i++)
+    {
+        if (DEBUG)
+            cout << "line53" << endl;
+        if (infx[i] == '(')
+        {
             st.push('(');
         }
-        else if(infx[i]==')'){
-            while(st.top()!='('){
-                if(DEBUG) cout<<"line59"<<endl;
+        else if (infx[i] == ')')
+        {
+            while (st.top() != '(')
+            {
+                if (DEBUG)
+                    cout << "line59" << endl;
                 post.push_back(st.top());
                 st.pop();
             }
             st.pop();
         }
-        else if (is_op(infx[i])){
-            while (get_prec(st.top())>=get_prec(infx[i])){
+        else if (is_op(infx[i]))
+        {
+            while (get_prec(st.top()) >= get_prec(infx[i]))
+            {
                 post.push_back(st.top());
                 st.pop();
             }
             st.push(infx[i]);
         }
-        else {
+        else
+        {
             post.push_back(infx[i]);
         }
     }
